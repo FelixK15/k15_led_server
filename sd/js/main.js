69cddb838cd1ad4e7027901fb10a878044eed5a8
Felix Klinge <felix@keencore.com>
2019-01-16 00:47:08 +0100

    send post request in js

--- sd/js/main.js
+++ sd/js/main.js
@@ -2,10 +2,12 @@ function onLoginClicked()
 {
     var username = document.getElementById("k15_username").value;
     var password = document.getElementById("k15_password").value;
+    var params = "user=" + username + "&password=" + password;
 
     var request = new XMLHttpRequest();
-    request.open('GET', "", true);
-    request.send(null);
+    request.open('POST', "bla", true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.send(params);
 
     request.onreadystatechange = function(e) {
         alert(request.readyState);
